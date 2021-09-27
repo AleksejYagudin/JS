7a463601e407e0fbf2a53b6178010847bd32e9a7
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2021-09-27 09:20:04 +0400

    upload-img

---
+++ UploadImg/index.php
@@ -0,0 +1,9 @@
+<?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");?>
+<link rel="stylesheet" href="style.css">
+<div class="container">
+    <div class="card">
+        <input type="file" id="file">
+    </div>
+</div>
+<script src="script.js"></script>
+<?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");?>
